4166bb7a8f848f56e0ab0dbfd59c09e98bbf33fb
wewillbefree <wewillbefree404@gmail.com>
2026-01-15 23:58:55 +0300

    day 8 done

--- script.js
+++ script.js
@@ -3,28 +3,32 @@ const buttonMinus = document.querySelector(".card__button_minus");
 const cardText = document.querySelector(".card__text")
 
 const state = {
-    value: 0
-
+    value: 0,
+    min: 0,
+    max: 20,
+    step: 1
 }
 
 
 const updateUi = () => {
-   buttonMinus.classList.toggle('card__button_disabled', state.value === 0)
+   buttonPlus.classList.toggle('card__button_disabled', state.value === state.max)
+   buttonMinus.classList.toggle('card__button_disabled', state.value === state.min)
    cardText.classList.toggle('card__text-red', state.value >= 10)
+   cardText.textContent = `Value: ${state.value}`;
+}
+
+const setValue = (newValue) => {
+    state.value = Math.max(state.min, Math.min(state.max, newValue))
+    updateUi()
+
 }
 
 buttonPlus.addEventListener('click', () => {
-    state.value++;
-    cardText.textContent = `Value: ${state.value}`;
-    updateUi()
+    setValue(state.value + state.step)
 })
 
 buttonMinus.addEventListener('click', () => {
-    if (state.value === 0) return
-    state.value--
-    cardText.textContent = `Value: ${state.value}`;
-    updateUi()
-
+     setValue(state.value - state.step)
 })
 
 updateUi()
